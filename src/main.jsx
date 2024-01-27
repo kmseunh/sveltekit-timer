57babb330b0054cbf70b158c4f720c2a61eaa2a6
kmseunh <tmdgus8779@gmail.com>
2024-01-27 17:51:02 +0900

    feat: 클라이언트 사이드 라우팅을 위해 react-router-dom 통합
    - 클라이언트 사이드 라우팅을 위해 react-router-dom을 통합
    - 타이머, 스톱워치 및 알람 페이지의 경로 정의
    
    change:
    - react-router-dom 구성 요소 및 createBrowserRouter 함수를 가져옴
    - 다른 페이지 (타이머, 스톱워치, 알람)의 경로 정의

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Timer from './pages/Timer.jsx';
+import Stopwatch from './pages/Stopwatch.jsx';
+import Alarm from './pages/Alarm.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/timer',
+        element: <Timer />,
+    },
+    {
+        path: '/stopwatch',
+        element: <Stopwatch />,
+    },
+    {
+        path: '/alarm',
+        element: <Alarm />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
