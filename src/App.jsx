d1d4c8a7258bbb6090e1c344a1c32b4dfcb7548f
kmseunh <tmdgus8779@gmail.com>
2024-01-27 17:52:20 +0900

    feat: Button 컴포넌트를 App 컴포넌트에 통합
    - UI 기능을 위해 Button 컴포넌트를 App 컴포넌트에 통합
    - 타이머, 스톱워치 및 알람 네비게이션을 위한 Button 컴포넌트 추가
    
    change:
    - App 컴포넌트에 Button 컴포넌트를 가져옴
    - 타이머, 스톱워치 및 알람 네비게이션을 위한 Button 컴포넌트 추가
    
    Related: #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import Button from './components/Button';
+
 function App() {
-    return <h1>Hello</h1>;
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <div className='w-[38rem] h-[54.5rem] bg-rec rounded-[1.875rem] relative'>
+                <div className='text-7xl font-semibold  relative flex items-center justify-center mt-[11.0625rem] uppercase'>
+                    kim&apos;s time
+                </div>
+                <div className='flex items-center justify-center mt-[5.875rem]'>
+                    <Button
+                        to={'/timer'}
+                        className='bg-btn w-[13.8125rem] '
+                        btnName={'timer'}
+                    />
+                </div>
+                <div className='flex items-center justify-center mt-[2.125rem]'>
+                    <Button
+                        to={'/stopwatch'}
+                        className='bg-rec text-btn border-btn border-2 w-[20.1875rem] '
+                        btnName={'stopwatch'}
+                    />
+                </div>
+                <div className='flex items-center justify-center mt-[2.125rem]'>
+                    <Button
+                        to={'/alarm'}
+                        className='bg-btn w-[13.8125rem] '
+                        btnName={'alarm'}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
