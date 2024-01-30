34c5860f2ed6ee29e36cbc7358b8dd67eaebe1e1
kmseunh <tmdgus8779@gmail.com>
2024-01-31 01:42:28 +0900

    feat: Implement UI layout with buttons for timer, stopwatch, and alarm
    - Button 컴포넌트를 lib/components/Button.svelte에서 import함
    - 화면 가운데 정렬된 플렉스 컨테이너에 배치된 UI 레이아웃 추가
    - 버튼 컴포넌트를 사용하여 타이머, 스톱워치, 알람으로 이동하는 버튼 추가
    - 버튼에는 to prop을 통해 이동할 경로, btnName prop을 통해 버튼 텍스트, className prop을 통해 사용자 정의 클래스 적용
    
    Related: #19
    Closed: #19

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Button from '$lib/components/Button.svelte';
+</script>
+
+<div class="flex items-center justify-center h-screen">
+    <div class="bg-rec w-[38rem] h-[54.5rem] rounded-[1.875rem] relative">
+        <div
+            class="text-7xl font-semibold justify-center items-center flex mt-[11.0625rem] uppercase relative"
+        >
+            kim’s time
+        </div>
+        <div class="flex items-center justify-center mt-[5.875rem]">
+            <Button
+                to="/timer"
+                btnName="timer"
+                className="bg-btn w-[13.8125rem]"
+            />
+        </div>
+        <div class="flex items-center justify-center mt-[2.125rem]">
+            <Button
+                to="/stopwatch"
+                btnName="stopwatch"
+                className="bg-rec text-btn border-btn border-2 w-[20.1875rem]"
+            />
+        </div>
+        <div class="flex items-center justify-center mt-[2.175rem]">
+            <Button
+                to="/alarm"
+                btnName="alarm"
+                className="bg-btn w-[13.8125rem]"
+            />
+        </div>
+    </div>
+</div>
