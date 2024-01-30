680fb988c04ae9c119810c10a5f27f9b8aba0318
kmseunh <tmdgus8779@gmail.com>
2024-01-31 01:41:57 +0900

    feat: Add button component with navigation functionality
    - 버튼 컴포넌트에 내비게이션 기능 추가
    - 버튼 클릭 시 지정된 경로로 이동하는 handleClick 함수 구현
    - 버튼의 모양과 스타일을 설정한 클래스 적용
    - btnName과 className을 통해 버튼 텍스트와 사용자 지정 클래스 동적으로 설정
    
    Related: #19

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    export let btnName;
+    export let className;
+    export let to;
+
+    const handleClick = () => {
+        goto(to);
+    };
+</script>
+
+<button
+    class="rounded-[70px] h-[5.625rem] pt-5 pr-14 pb-5 pl-14 flex flex-row gap-2.5 items-center justify-center relative overflow-hidden font-light uppercase text-[2.625rem] {className}"
+    on:click={handleClick}
+>
+    {btnName}
+</button>
